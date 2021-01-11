6419d228456d22f95e6b3f2b6264583289e54d04
fabiobarreto_pro <fabiobarreto_pro@hotmail.com>
2021-01-11 20:56:24 -0300

    models

--- agenda/contatos/models.py
+++ agenda/contatos/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.utils import timezone
+"""
+CONTATOS
+nome: STR * (obrigatório)
+sobrenome: STR (opcional)
+telefone: STR * (obrigatório)
+email: STR (opcional)
+data_criacao: DATETIME (automático)
+descricao: texto
+categoria: CATEGORIA (outro model)
 
-# Create your models here.
+CATEGORIA
+nome: STR * (obrigatório)
+
+"""
+
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=255)
+
+
+class Contato(models.Model):
+    nome = models.CharField(max_length=255)
+    sobrenome = models.CharField(max_length=255, blank=True)  # Para dizer que é opicional
+    telefone = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, blank=True)  # Para dizer que é opicional
+    data_criacao = models.DateTimeField(default=timezone.now)
+    descricao = models.TextField(blank=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+
+
+
