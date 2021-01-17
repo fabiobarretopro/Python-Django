f74b525661297eb1a9ea703aa54623cd2f25f5ba
fabiobarreto_pro <fabiobarreto_pro@hotmail.com>
2021-01-17 18:11:53 -0300

    admin e models

--- agenda/contatos/models.py
+++ agenda/contatos/models.py
@@ -19,15 +19,21 @@ nome: STR * (obrigatório)
 class Categoria(models.Model):
     nome = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.nome
+
 
 class Contato(models.Model):
     nome = models.CharField(max_length=255)
-    sobrenome = models.CharField(max_length=255, blank=True)  # Para dizer que é opicional
+    sobrenome = models.CharField(max_length=255, blank=True)  # Para dizer que é opcional
     telefone = models.CharField(max_length=255)
-    email = models.CharField(max_length=255, blank=True)  # Para dizer que é opicional
+    email = models.CharField(max_length=255, blank=True)  # Para dizer que é opcional
     data_criacao = models.DateTimeField(default=timezone.now)
     descricao = models.TextField(blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.nome
 
 
 
